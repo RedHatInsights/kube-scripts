d6c0c7a7a80833aa388a3f6853241b89a0a8edd4
Kyle Lape <klape@redhat.com>
2019-05-16 09:15:31 -0500

    Move over more code to the kube_api module

--- oclogs.py
+++ oclogs.py
@@ -2,17 +2,13 @@
 
 import logging
 import os
-import json
-import requests
 import click
-import time
-import random
 import arrow
 
 from threading import Thread
 
 from util import kube_api as kube
-from util.kube_api import crayons, DATE_FORMAT
+from util.kube_api import crayons, DATE_FORMAT, Observer
 
 
 try:
@@ -21,28 +17,6 @@ except Exception:
     pass
 
 logging.basicConfig()
-
-
-class Observer(object):
-
-    def __init__(self, since=arrow.now().shift(minutes=-1), slack=None):
-        self.since = since
-        self.slack = slack
-        self.seen_messages = {}
-
-    def has_been_seen(self, msg_key, now):
-        return (msg_key in self.seen_messages and  # noqa: W504
-                self.seen_messages[msg_key] > self.since)
-
-    def clear_seen_messages(self):
-        if random.randint(0, 100) == 1:
-            hour_ago = arrow.now().shift(hours=-1)
-            for k, v in list(self.seen_messages.items()):
-                if v < hour_ago:
-                    del self.seen_messages[k]
-
-    def observe(self, resource, feed):
-        pass
 
 
 class Console(Observer):
@@ -141,61 +115,6 @@ class PodOOM(Observer):
         print(f"Container: {c.name}")
         print(f"Killed: {killed.format(DATE_FORMAT)}")
         print(crayons.white("*" * 80))
-
-
-class OpenshiftFeed(object):
-
-    resource = None
-    api_suffix = None
-
-    def __init__(self, api, headers, namespace, observers, ca_store):
-        self.api = api
-        self.headers = headers
-        self.namespace = namespace
-        self.observers = observers
-        self.ca_store = ca_store
-        self.resources = {}
-
-    def fetch_loop(self):
-        while True:
-            try:
-                ns_url = f"namespaces/{self.namespace}/" if self.namespace else ""
-
-                kwargs = {"headers": self.headers, "stream": True}
-                if self.ca_store:
-                    kwargs["verify"] = self.ca_store
-
-                r = requests.get(f"{self.api}/watch/{ns_url}{self.api_suffix}", **kwargs, verify=False)
-
-                if r.status_code != 200:
-                    print(f"Invalid status from server: %s\n%s" % (
-                        r.status_code,
-                        r.json()['message']
-                    ))
-                    return
-
-                for l in r.iter_lines():
-                    d = json.loads(l)
-                    resource = self.resource(d["object"])
-                    self.resources[resource.name] = resource
-                    for o in self.observers:
-                        o.observe(resource, self)
-            except Exception:
-                logging.exception("Failed connection")
-            print("Reconnecting...")
-            time.sleep(1)
-
-
-class PodFeed(OpenshiftFeed):
-
-    resource = kube.Pod
-    api_suffix = "pods"
-
-
-class EventFeed(OpenshiftFeed):
-
-    resource = kube.Event
-    api_suffix = "events"
 
 
 class Slack(object):
